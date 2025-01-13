f982b7eb40597f06aeeb20660a8bbc47660219ef
jmcho2010 <projm2786@gmail.com>
2025-01-13 15:49:26 +0900

    test

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ const router = express.Router();
 
 // 실질적 요청에 대한 응답은 라우팅에서 처리.
 router.get('/', (req, res) =>{
-    res.send('index page');
+    res.render('index', { // render 메서드를 통해 views의 .ejs파일로 연결이 가능.
+        title: 'Todo 애플리케이션'
+    })
 });
 
 router.get('/test', (req, res) =>{
-    res.send('test page');
+    res.render('gunchim', { // render 메서드를 통해 views의 .ejs파일로 연결이 가능.
+        title: 'Todo 애플리케이션'
+    })
 });
 
 module.exports = router;
